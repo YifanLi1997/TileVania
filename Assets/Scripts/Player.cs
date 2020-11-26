285b71816c168612eea4017cb9717c2e0f144188
Yifan <53184905+YifanLi1997@users.noreply.github.com>
2020-11-26 17:22:56 +0800

    Run and Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    // Config
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpForce = 10f;
+
+    // State
+    bool isAlive = true;
+
+    // Cached component references 
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        Jump();
+    }
+
+    private void Run()
+    {
+        float inputDirection = CrossPlatformInputManager.GetAxis("Horizontal");
+        rb.velocity = new Vector2(inputDirection * runSpeed, rb.velocity.y);
+
+        bool isRunning = Mathf.Abs(inputDirection) > Mathf.Epsilon;
+
+        if (isRunning)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
+
+        animator.SetBool("Running", isRunning);
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpForce);
+            rb.velocity += jumpVelocityToAdd;
+        }
     }
 }
